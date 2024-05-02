1fed9379173fc7fd60fddcda7581b5cb38f96733
lilas <lilas.merbouche@gmail.com>
2024-05-02 18:20:39 +0200

    update package version 0.1.6 -> 0.1.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="qiskit_scaleway",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     install_requires=[
         "qiskit==1.0.2",
