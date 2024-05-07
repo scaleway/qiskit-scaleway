59410ad8095191b51b4ac96cd50d1f3e15612945
Valentin Macheret <valentin.macheret.fr@gmail.com>
2024-05-07 17:18:39 +0200

    feat(ci): add auto tests

---
+++ tests/test_aer_simple_circuit.py
@@ -0,0 +1,40 @@
+import os
+
+from qiskit import QuantumCircuit
+from qiskit_scaleway import ScalewayProvider
+
+
+def test_aer_simple_circuit():
+    provider = ScalewayProvider(
+        project_id=os.environ["QISKIT_SCALEWAY_PROJECT_ID"],
+        secret_key=os.environ["QISKIT_SCALEWAY_API_TOKEN"],
+        url=os.environ["QISKIT_SCALEWAY_API_URL"],
+    )
+
+    backend = provider.get_backend("aer_simulation_2l4")
+
+    assert backend is not None
+
+    session_id = backend.start_session(
+        name="my-aer-session-autotest",
+        deduplication_id="my-aer-session-autotest",
+        max_duration="2m",
+    )
+
+    assert session_id is not None
+
+    try:
+        qc = QuantumCircuit(4)
+        qc.h(0)
+        qc.cx(0, 1)
+        qc.cx(0, 2)
+        qc.cx(0, 3)
+        qc.measure_all()
+
+        # Create and send a job to the target session
+        result = backend.run(qc, shots=1000, session_id=session_id).result()
+
+        assert result is not None
+        assert result.success
+    finally:
+        backend.stop_session(session_id)
