b6689e44e5788ee9ed6cbd3df37106bc65e1d24e
Valentin Macheret <valentin.macheret.fr@gmail.com>
2025-04-04 14:22:48 +0200

    feat(aqt): bootstrap AQT integration

---
+++ qiskit_scaleway/backends/aqt_backend.py
@@ -0,0 +1,172 @@
+# Copyright 2025 Scaleway
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import randomname
+import warnings
+
+from typing import Union, List
+
+from qiskit.providers import Options
+from qiskit.circuit import QuantumCircuit
+from qiskit.providers import convert_to_target
+from qiskit.providers.models import QasmBackendConfiguration
+from qiskit_aer.backends.aer_simulator import AerSimulator
+from qiskit_aer.backends.aerbackend import NAME_MAPPING
+
+from .aer_job import AerJob
+from .scaleway_backend import ScalewayBackend
+from ..utils import QaaSClient
+
+
+class AqtBackend(ScalewayBackend):
+    def __init__(
+        self,
+        provider,
+        client: QaaSClient,
+        backend_id: str,
+        name: str,
+        availability: str,
+        version: str,
+        num_qubits: int,
+        metadata: str,
+    ):
+        super().__init__(
+            provider=provider,
+            client=client,
+            backend_id=backend_id,
+            name=name,
+            availability=availability,
+            version=version,
+        )
+
+        self._options = self._default_options()
+
+        # Create Target
+        self._configuration = QasmBackendConfiguration.from_dict(
+            AerSimulator._DEFAULT_CONFIGURATION
+        )
+        self._properties = None
+        self._target = convert_to_target(
+            self._configuration, self._properties, None, NAME_MAPPING
+        )
+        self._target.num_qubits = num_qubits
+
+        # Set option validators
+        self.options.set_validator("shots", (1, 1e6))
+        self.options.set_validator("memory", bool)
+
+    def __repr__(self) -> str:
+        return f"<AerBackend(name={self.name},num_qubits={self.num_qubits},platform_id={self.id})>"
+
+    @property
+    def target(self):
+        return self._target
+
+    @property
+    def num_qubits(self) -> int:
+        return self._target.num_qubits
+
+    @property
+    def max_circuits(self):
+        return 1024
+
+    def run(
+        self, circuits: Union[QuantumCircuit, List[QuantumCircuit]], **run_options
+    ) -> AerJob:
+        if not isinstance(circuits, list):
+            circuits = [circuits]
+
+        job_config = dict(self._options.items())
+
+        for kwarg in run_options:
+            if not hasattr(self.options, kwarg):
+                warnings.warn(
+                    f"Option {kwarg} is not used by this backend",
+                    UserWarning,
+                    stacklevel=2,
+                )
+            else:
+                job_config[kwarg] = run_options[kwarg]
+
+        job_name = f"qj-aer-{randomname.get_name()}"
+
+        session_id = job_config.get("session_id", None)
+
+        job_config.pop("session_id")
+        job_config.pop("session_name")
+        job_config.pop("session_deduplication_id")
+        job_config.pop("session_max_duration")
+        job_config.pop("session_max_idle_duration")
+
+        job = AerJob(
+            backend=self,
+            client=self._client,
+            circuits=circuits,
+            config=job_config,
+            name=job_name,
+        )
+
+        if session_id in ["auto", None]:
+            session_id = self.start_session(name=f"auto-{self._options.session_name}")
+            assert session_id is not None
+
+        job.submit(session_id)
+
+        return job
+
+    @classmethod
+    def _default_options(self):
+        # https://qiskit.github.io/qiskit-aer/stubs/qiskit_aer.AerSimulator.html
+        return Options(
+            session_id="auto",
+            session_name="aqt-session-from-qiskit",
+            session_deduplication_id="aqt-session-from-qiskit",
+            session_max_duration="1h",
+            session_max_idle_duration="20m",
+            shots=1000,
+            memory=False,
+            seed_simulator=None,
+            method="automatic",
+            precision="double",
+            max_shot_size=None,
+            enable_truncation=True,
+            max_parallel_experiments=1,
+            zero_threshold=1e-10,
+            validation_threshold=1e-8,
+            accept_distributed_results=None,
+            runtime_parameter_bind_enable=False,
+            statevector_parallel_threshold=14,
+            statevector_sample_measure_opt=10,
+            stabilizer_max_snapshot_probabilities=32,
+            extended_stabilizer_sampling_method="resampled_metropolis",
+            extended_stabilizer_metropolis_mixing_time=5000,
+            extended_stabilizer_approximation_error=0.05,
+            extended_stabilizer_norm_estimation_samples=100,
+            extended_stabilizer_norm_estimation_repetitions=3,
+            extended_stabilizer_parallel_threshold=100,
+            extended_stabilizer_probabilities_snapshot_samples=3000,
+            matrix_product_state_max_bond_dimension=None,
+            matrix_product_state_truncation_threshold=1e-16,
+            mps_sample_measure_algorithm="mps_apply_measure",
+            mps_log_data=False,
+            mps_swap_direction="mps_swap_left",
+            chop_threshold=1e-8,
+            mps_parallel_threshold=14,
+            mps_omp_threads=1,
+            tensor_network_num_sampling_qubits=10,
+            use_cuTensorNet_autotuning=False,
+            fusion_enable=True,
+            fusion_verbose=False,
+            fusion_max_qubit=None,
+            fusion_threshold=None,
+        )
---
+++ qiskit_scaleway/backends/aqt_job.py
@@ -0,0 +1,131 @@
+# Copyright 2025 Scaleway
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import json
+
+from typing import Union, List
+
+from qiskit.providers import JobError
+from qiskit.result import Result
+from qiskit import qasm3
+
+from ..utils import QaaSClient
+from ..versions import USER_AGENT
+from .scaleway_job import ScalewayJob
+from .scaleway_models import (
+    JobPayload,
+    ClientPayload,
+    BackendPayload,
+    RunPayload,
+    SerializationType,
+    CircuitPayload,
+)
+
+
+class AqtJob(ScalewayJob):
+    def __init__(
+        self,
+        name: str,
+        backend,
+        client: QaaSClient,
+        circuits,
+        config,
+    ) -> None:
+        super().__init__(name, backend, client)
+        self._circuits = circuits
+        self._config = config
+
+    def submit(self, session_id: str) -> None:
+        if self._job_id:
+            raise RuntimeError(f"Job already submitted (ID: {self._job_id})")
+
+        options = self._config.copy()
+
+        run_opts = RunPayload(
+            options={
+                "shots": options.pop("shots"),
+                "memory": options.pop("memory"),
+                "seed_simulator": options.pop("seed_simulator"),
+            },
+            circuits=list(
+                map(
+                    lambda c: CircuitPayload(
+                        serialization_type=SerializationType.QASM_V3,
+                        circuit_serialization=qasm3.dumps(c),
+                    ),
+                    self._circuits,
+                )
+            ),
+        )
+
+        backend_opts = BackendPayload(
+            name=self.backend().name,
+            version=self.backend().version,
+            options=options,
+        )
+
+        client_opts = ClientPayload(
+            user_agent=USER_AGENT,
+        )
+
+        job_payload = JobPayload.schema().dumps(
+            JobPayload(
+                backend=backend_opts,
+                run=run_opts,
+                client=client_opts,
+            )
+        )
+
+        self._job_id = self._client.create_job(
+            name=self._name,
+            session_id=session_id,
+            circuits=job_payload,
+        )
+
+    def result(
+        self, timeout=None, fetch_interval: int = 3
+    ) -> Union[Result, List[Result]]:
+        if self._job_id == None:
+            raise JobError("Job ID error")
+
+        job_results = self._wait_for_result(timeout, fetch_interval)
+
+        def __make_result_from_payload(payload: str) -> Result:
+            payload_dict = json.loads(payload)
+
+            return Result.from_dict(
+                {
+                    "results": payload_dict["results"],
+                    "backend_name": payload_dict["backend_name"],
+                    "backend_version": payload_dict["backend_version"],
+                    "job_id": self._job_id,
+                    "qobj_id": ", ".join(x.name for x in self._circuits),
+                    "success": payload_dict["success"],
+                    "header": payload_dict.get("header"),
+                    "metadata": payload_dict.get("metadata"),
+                }
+            )
+
+        qiskit_results = list(
+            map(
+                lambda r: __make_result_from_payload(
+                    self._extract_payload_from_response(r)
+                ),
+                job_results,
+            )
+        )
+
+        if len(qiskit_results) == 1:
+            return qiskit_results[0]
+
+        return qiskit_results
