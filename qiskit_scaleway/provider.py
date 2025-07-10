7ef3b544d2c58146c649c521bbc4f54a5230ff55
Valentin Macheret <valentin.macheret.fr@gmail.com>
2025-07-10 15:53:47 +0200

    feat(qiskit): fix provider

--- qiskit_scaleway/provider.py
+++ qiskit_scaleway/provider.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 import os
 
-from dotenv import dotenv_values
 from typing import Optional, List, Dict
 
 from qiskit.providers.providerutils import filter_backends
@@ -25,11 +24,11 @@ from scaleway_qaas_client import QaaSClient, QaaSPlatform
 
 class ScalewayProvider:
     """
-    :param project_id: optional UUID of the Scaleway Project, if the provided ``project_id`` is None, the value is loaded from the SCALEWAY_PROJECT_ID variables in the dotenv file or the QISKIT_SCALEWAY_PROJECT_ID environment variables
+    :param project_id: optional UUID of the Scaleway Project, if the provided ``project_id`` is None, the value is loaded from the QISKIT_SCALEWAY_PROJECT_ID environment variables
 
-    :param secret_key: optional authentication token required to access the Scaleway API, if the provided ``secret_key`` is None, the value is loaded from the SCALEWAY_API_TOKEN variables in the dotenv file or the QISKIT_SCALEWAY_API_TOKEN environment variables
+    :param secret_key: optional authentication token required to access the Scaleway API, if the provided ``secret_key`` is None, the value is loaded from the QISKIT_SCALEWAY_API_SECRET_KEY environment variables
 
-    :param url: optional value, endpoint URL of the API, if the provided ``url`` is None, the value is loaded from the SCALEWAY_API_URL variables in the dotenv file or the QISKIT_SCALEWAY_API_URL environment variables, if no url is found, then ``_ENDPOINT_URL`` is used.
+    :param url: optional value, endpoint URL of the API, if the provided ``url`` is None, the value is loaded from the QISKIT_SCALEWAY_API_URL environment variables
     """
 
     def __init__(
@@ -38,29 +37,19 @@ class ScalewayProvider:
         secret_key: Optional[str] = None,
         url: Optional[str] = None,
     ) -> None:
-        env_token = dotenv_values().get("QISKIT_SCALEWAY_API_TOKEN") or os.getenv(
-            "QISKIT_SCALEWAY_API_TOKEN"
-        )
-        env_project_id = dotenv_values().get("QISKIT_SCALEWAY_PROJECT_ID") or os.getenv(
-            "QISKIT_SCALEWAY_PROJECT_ID"
-        )
-        env_api_url = dotenv_values().get("QISKIT_SCALEWAY_API_URL") or os.getenv(
-            "QISKIT_SCALEWAY_API_URL"
-        )
+        secret_key = secret_key or os.getenv("QISKIT_SCALEWAY_API_SECRET_KEY")
+        project_id = project_id or os.getenv("QISKIT_SCALEWAY_PROJECT_ID")
+        url = os.getenv("QISKIT_SCALEWAY_API_URL")
 
-        token = secret_key or env_token
-
-        if token is None:
+        if secret_key is None:
             raise Exception("secret_key is missing")
-
-        project_id = project_id or env_project_id
 
         if project_id is None:
             raise Exception("project_id is missing")
 
-        api_url = url or env_api_url
-
-        self.__client = QaaSClient(url=api_url, token=token, project_id=project_id)
+        self.__client = QaaSClient(
+            url=url, secret_key=secret_key, project_id=project_id
+        )
 
     def get_backend(self, name=None, **kwargs):
         """Return a single backend matching the specified filtering.
